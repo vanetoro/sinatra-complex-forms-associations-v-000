758b8fd847e99b31d379ca81cdc0efc7f41699f6
Vanessa Toro <vanetoro84@gmail.com>
2018-06-06 22:04:49 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -29,7 +29,6 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(name: params[:pet_name])
-    binding.pry
     @pet.update(owner: params['owner']['name'])
   end
 
